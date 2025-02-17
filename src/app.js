e2e0fa138c642425a0862b35462aab1a6c25fa90
Georgi <ridriruners62@gmail.com>
2025-02-17 13:18:39 +0000

    Add routing.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log('Hello Boss');
+import { page } from "./library/page.js";
+
+
+page('/', homeView);
+page('/register', registerView);
+page('/login', loginView);
+page('/logout', logoutView);
+page('/create', createView);
+page('/dashboard', dashboardView);
+page('/details/:id', detailsView);
+page('/edit/:id', editView);
+page('/delete/:id', deleteView);
+page('/like', likeView);
+
+page();

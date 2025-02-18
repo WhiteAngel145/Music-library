1d94f5993e0bac6e946e772a0f5dcd219e9c9efb
Georgi <ridriruners62@gmail.com>
2025-02-18 11:42:46 +0000

    Add detailsView.js
    Set correct navigation in details page.

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -2,9 +2,15 @@ import { api } from '../apis/requester.js';
 
 const endpoint = {
 	dashboard: '/data/albums?sortBy=_createdOn%20desc',
+	details: '/data/albums/'
 }
 
 export async function dashboard() {
 	const result = await api.get(endpoint.dashboard);
+	return result;
+}
+
+export async function getDetails(_id) {
+	const result = await api.get(endpoint.details + _id)
 	return result;
 }
--- src/views/detailView.js
+++ src/views/detailView.js
@@ -1,32 +1,49 @@
-import { render, html } from '../lib/lit-html.js';
+import { render, html, nothing } from '../lib/lit-html.js';
+import { getDetails } from '../services/dataService.js';
+import { getUserData } from '../utils/userUtils.js';
 
-const template = () => html`
+const template = (data, isOwner, userData) => html`
     <section id="details">
       <div id="details-wrapper">
         <p id="details-title">Album Details</p>
         <div id="img-wrapper">
-          <img src="./images/BackinBlack.jpeg" alt="example1" />
+          <img src=${data.imageUrl} alt=${data.album} />
         </div>
         <div id="info-wrapper">
-          <p><strong>Band:</strong><span id="details-singer">AC/DC</span></p>
+          <p><strong>Band:</strong><span id="details-singer">${data.singer}</span></p>
           <p>
-            <strong>Album name:</strong><span id="details-album">Back in Black</span>
+            <strong>Album name:</strong><span id="details-album">${data.album}</span>
           </p>
-          <p><strong>Release date:</strong><span id="details-release">1980</span></p>
-          <p><strong>Label:</strong><span id="details-label">Epic</span></p>
-          <p><strong>Sales:</strong><span id="details-sales">26 million (50 million claimed)</span></p>
+          <p><strong>Release date:</strong><span id="details-release">${data.release}</span></p>
+          <p><strong>Label:</strong><span id="details-label">${data.label}</span></p>
+          <p><strong>Sales:</strong><span id="details-sales">${data.sales}</span></p>
         </div>
         <div id="likes">Likes: <span id="likes-count">0</span></div>
         <!--Edit and Delete are only for creator-->
         <div id="action-buttons">
-          <a href="" id="like-btn">Like</a>
-          <a href="" id="edit-btn">Edit</a>
-          <a href="" id="delete-btn">Delete</a>
+          ${userData
+          ? !isOwner
+            ? html`<a href="/like${data._id}" id="like-btn">Like</a>`
+            : html`
+                  <a href="/edit${data._id}" id="edit-btn">Edit</a>
+                  <a href="/delete${data._id}" id="delete-btn">Delete</a>
+                  `
+          : nothing
+          }
         </div>
       </div>
     </section>
 `;
 
 export async function detailsView(ctx) {
-	render(template());
+  const idAlbum = ctx.params.id;
+  const detailsAlbum = await getDetails(idAlbum);
+  const userData = getUserData();
+  const isOwner = userData && userData._id === detailsAlbum._ownerId;
+
+  console.log('Album Details --------> ', detailsAlbum);
+  console.log('userData --------> ', userData);
+  
+  
+	render(template(detailsAlbum, isOwner, userData));
 }
